04ff3ed0d10d7014da2867fe53c052c7fffe1077
tingzcb <tingting.sun@cqumail.com>
2024-03-20 02:04:54 +1100

    add login logo image

--- src/components/modeToggle.tsx
+++ src/components/modeToggle.tsx
@@ -3,7 +3,6 @@ import React from "react";
 import { Moon, Sun } from "lucide-react";
 import { useTheme } from "next-themes";
 import { Button } from "@/components/ui/button";
-import { Switch } from "./ui/switch";
 import {
   DropdownMenu,
   DropdownMenuContent,
