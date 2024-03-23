62a5bda82fb4d8d9fbe236539a2cc272309c8a3d
tingzcb <tingting.sun@cqumail.com>
2024-03-23 23:14:03 +1100

    finish Blog CardList and Card Component

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,33 +1,42 @@
+import Link from "next/link";
 import React from "react";
 
 type Props = {};
 
 const Cards = (props: Props) => {
   return (
-    <div className="grid grid-cols-2 max-w-3xl p-4 bg-slate-400 my-2 rounded-lg">
+    <div className="grid grid-cols-2 max-w-3xl p-4 my-2 rounded-lg gap-8">
       <div className="imageContainer">
         <img
-          src="/tiktok.png"
+          src="/Island.jpg"
           alt="recent post image"
-          className="rounded-lg w-80 h-80"
+          className="rounded-lg w-110 h-80"
         ></img>
       </div>
 
       <div className="textContainer">
-        <p>
+        <div className="detail">
+          <span>11.03.24 - </span>
+          <span className="text-red-500">CULTURE</span>
+        </div>
+
+        <h1 className="py-4 text-2xl font-semibold font-mono ">
+          Lorem ipsum dolor sit amet quis sint delectus
+        </h1>
+        <p className=" text-gray-500">
           Lorem ipsum dolor sit amet consectetur adipisicing elit. Id, inventore
           voluptatibus quis sint delectus dignissimos, amet dolorum vero culpa,
-          laudantium ducimus rem fugiat! Earum quidem dolor velit! Quis mollitia
-          quisquam, quia vero accusamus dignissimos corrupti ab amet culpa
-          molestias expedita impedit doloribus, veniam architecto velit hic
-          ipsa? Maiores ipsum repellat accusantium dignissimos facilis
-          consequuntur, non perspiciatis rem pariatur quisquam cumque quibusdam
-          debitis beatae, itaque sapiente quidem. Magni cupiditate maxime,
-          officiis veniam ad aspernatur recusandae voluptates ratione tempora
-          debitis commodi iure nobis numquam reprehenderit odio laborum
-          accusantium temporibus velit. Magni ex repellat dolore vitae minima
-          nulla velit atque aliquam facere enim.
+          laudantium ducimus rem fugiat!
         </p>
+
+        <div className="py-4">
+          <Link href={"/blog"}>
+            <p className="underline underline-offset-4 font-semi  hover:-translate-y-[2px]">
+              {" "}
+              Read More
+            </p>
+          </Link>
+        </div>
       </div>
     </div>
   );
