f7ef252bd65a27ccacc681b6ca84b9e4b89d7344
tingzcb <tingting.sun@cqumail.com>
2024-03-23 20:29:33 +1100

    create CategoryList Component, CardList Component, Card Component.

--- src/components/CategoryList.tsx
+++ src/components/CategoryList.tsx
@@ -1,9 +1,98 @@
+import Link from "next/link";
 import React from "react";
 
 type Props = {};
 
 const CategoryList = (props: Props) => {
-  return <div>CategoryList</div>;
+  return (
+    <div className="container">
+      <h1 className=" py-4 text-2xl font-semibold font-mono">
+        Popular Categories
+      </h1>
+      <div className="categories grid grid-cols-6 gap-4">
+        <div className="category bg-slate-500 w-fit py-2 px-4 rounded-md">
+          <Link
+            href={"/blog？cat=style"}
+            className="flex flex-row gap-2 text-lg font-semibold font-mono"
+          >
+            <img
+              className="h-[2.2rem] w-[2.2rem] rounded-full object-cover object-center"
+              src="/facebook.png"
+              alt="facebook image"
+            />
+            Food
+          </Link>
+        </div>
+
+        <div className="category bg-slate-500 w-fit py-2 px-4 rounded-md">
+          <Link
+            href={"/blog？cat=style"}
+            className="flex flex-row gap-2 text-lg font-semibold font-mono"
+          >
+            <img
+              className="h-[2.2rem] w-[2.2rem] rounded-full object-cover object-center"
+              src="/facebook.png"
+              alt="facebook image"
+            />
+            Food
+          </Link>
+        </div>
+
+        <div className="category bg-slate-500 w-fit py-2 px-4 rounded-md">
+          <Link
+            href={"/blog？cat=style"}
+            className="flex flex-row gap-2 text-lg font-semibold font-mono"
+          >
+            <img
+              className="h-[2.2rem] w-[2.2rem] rounded-full object-cover object-center"
+              src="/facebook.png"
+              alt="facebook image"
+            />
+            Food
+          </Link>
+        </div>
+        <div className="category bg-slate-500 w-fit py-2 px-4 rounded-md">
+          <Link
+            href={"/blog？cat=style"}
+            className="flex flex-row gap-2 text-lg font-semibold font-mono"
+          >
+            <img
+              className="h-[2.2rem] w-[2.2rem] rounded-full object-cover object-center"
+              src="/facebook.png"
+              alt="facebook image"
+            />
+            Food
+          </Link>
+        </div>
+        <div className="category bg-slate-500 w-fit py-2 px-4 rounded-md">
+          <Link
+            href={"/blog？cat=style"}
+            className="flex flex-row gap-2 text-lg font-semibold font-mono"
+          >
+            <img
+              className="h-[2.2rem] w-[2.2rem] rounded-full object-cover object-center"
+              src="/facebook.png"
+              alt="facebook image"
+            />
+            Food
+          </Link>
+        </div>
+        <div className="category bg-slate-500 w-fit py-2 px-4 rounded-md">
+          <Link
+            href={"/blog？cat=style"}
+            className="flex flex-row gap-2 text-lg font-semibold font-mono"
+          >
+            <img
+              className="h-[2.2rem] w-[2.2rem] rounded-full object-cover object-center"
+              src="/facebook.png"
+              alt="facebook image"
+            />
+            Food
+          </Link>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default CategoryList;
