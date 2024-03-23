07680096e1d2160fd5dee997a3269bba13a8328f
tingzcb <tingting.sun@cqumail.com>
2024-03-23 23:14:03 +1100

    finish Blog CardList and Card Component

--- src/components/CategoryList.tsx
+++ src/components/CategoryList.tsx
@@ -6,11 +6,25 @@ type Props = {};
 const CategoryList = (props: Props) => {
   return (
     <div className="container">
-      <h1 className=" py-4 text-2xl font-semibold font-mono">
+      <h1 className=" py-4 text-2xl font-semibold font-mono ">
         Popular Categories
       </h1>
       <div className="categories grid grid-cols-6 gap-4">
-        <div className="category bg-slate-500 w-fit py-2 px-4 rounded-md">
+        <div className="category bg-slate-500 w-fit py-2 px-4 rounded-md hover:-translate-y-[2px]">
+          <Link
+            href={"/blog？cat=style"}
+            className="flex flex-row gap-2 text-lg font-semibold font-mono "
+          >
+            <img
+              className="h-[2.2rem] w-[2.2rem] rounded-full object-cover object-center"
+              src="/facebook.png"
+              alt="facebook image"
+            />
+            Food
+          </Link>
+        </div>
+
+        <div className="category hover:-translate-y-[2px] bg-slate-500 w-fit py-2 px-4 rounded-md">
           <Link
             href={"/blog？cat=style"}
             className="flex flex-row gap-2 text-lg font-semibold font-mono"
@@ -24,7 +38,7 @@ const CategoryList = (props: Props) => {
           </Link>
         </div>
 
-        <div className="category bg-slate-500 w-fit py-2 px-4 rounded-md">
+        <div className="category hover:-translate-y-[2px] bg-slate-500 w-fit py-2 px-4 rounded-md">
           <Link
             href={"/blog？cat=style"}
             className="flex flex-row gap-2 text-lg font-semibold font-mono"
@@ -37,8 +51,7 @@ const CategoryList = (props: Props) => {
             Food
           </Link>
         </div>
-
-        <div className="category bg-slate-500 w-fit py-2 px-4 rounded-md">
+        <div className="category hover:-translate-y-[2px]  bg-slate-500 w-fit py-2 px-4 rounded-md">
           <Link
             href={"/blog？cat=style"}
             className="flex flex-row gap-2 text-lg font-semibold font-mono"
@@ -51,7 +64,7 @@ const CategoryList = (props: Props) => {
             Food
           </Link>
         </div>
-        <div className="category bg-slate-500 w-fit py-2 px-4 rounded-md">
+        <div className="category hover:-translate-y-[2px] bg-slate-500 w-fit py-2 px-4 rounded-md">
           <Link
             href={"/blog？cat=style"}
             className="flex flex-row gap-2 text-lg font-semibold font-mono"
@@ -64,20 +77,7 @@ const CategoryList = (props: Props) => {
             Food
           </Link>
         </div>
-        <div className="category bg-slate-500 w-fit py-2 px-4 rounded-md">
-          <Link
-            href={"/blog？cat=style"}
-            className="flex flex-row gap-2 text-lg font-semibold font-mono"
-          >
-            <img
-              className="h-[2.2rem] w-[2.2rem] rounded-full object-cover object-center"
-              src="/facebook.png"
-              alt="facebook image"
-            />
-            Food
-          </Link>
-        </div>
-        <div className="category bg-slate-500 w-fit py-2 px-4 rounded-md">
+        <div className="category hover:-translate-y-[2px] bg-slate-500 w-fit py-2 px-4 rounded-md">
           <Link
             href={"/blog？cat=style"}
             className="flex flex-row gap-2 text-lg font-semibold font-mono"
