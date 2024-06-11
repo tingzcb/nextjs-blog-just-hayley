6c9eca008ab833bf6677ad0b763642a573aa7667
tingzcb <tingting.sun@cqumail.com>
2024-06-11 21:51:30 +1000

    update featured page

--- src/components/Featured.tsx
+++ src/components/Featured.tsx
@@ -9,13 +9,13 @@ const Featured = (props: Props) => {
   return (
     <div className="container  my-0 mx-auto  text-center">
       <h1 className="title p-6 text-4xl">
-        <b>Hey, hayley&apos;s blogs are here!</b> Discover my creative designs
+        <b>Hey, Tina&apos;s blogs are here!</b> Discover my creative development
         please!
       </h1>
       <div className="post grid grid-cols-3 gap-4">
         <div className="imgContainer ">
           <img
-            src="/Island.jpg"
+            src="/glass.jpeg"
             alt="feature image"
             className="rounded-lg w-120 h-120"
           ></img>
@@ -24,28 +24,31 @@ const Featured = (props: Props) => {
         <div className="txtContainer col-span-2  ">
           <h1 className="postTitle p-6  ">
             {" "}
-            Lorem ipsum dolor sit, amet consectetur adipisicing elit.
+            Code with passion, learn with curiosity, and create with purpose.
           </h1>
           <p className="postDesc ">
             {" "}
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. At
-            provident repellat, et veritatis asperiores quae unde dolore ab
-            officia int sapiente sequi! Ipsum, iusto. Quisquam praesentium
-            voluptates asperiores a d eum! Unde ipsa praesentium nam laborum
-            assumenda itaque, atque vero nulla in cidunt cumque omnis.
-            Accusantium amet ea voluptatem! Quia ducimus ut magni doloribus
-            corporis. Delectus dolore eveniet tenetur vero illum modi ab, vel it
-            porro blanditiis minima, quia, provident ipsum! Amet nihil impedit
-            quaera t ipsum delectus praesentium ad iure. Eveniet impedit at
-            veniam quasi dignis simos explicabo quaerat ipsam tempore nesciunt
-            repellendus, temporibus quis q uae fugiat necessitatibus quia enim
-            id odit repellat eius. Aperiam sunt atque totam minus esse, optio
-            odio nobis, laborum dolores nisi facilis. Hic velit dolorum autem
-            distinctio pariatur ipsam.
+            Hello there! I&apos;m Tina, a passionate Full Stack Developer with a
+            knack for solving complex problems and a love for crafting
+            efficient, scalable web applications. My journey in the tech world
+            began with a Bachelor&apos;s degree in Software Engineering from
+            University and took flight with a Master&apos;s in Information
+            Technology from Central Queensland University, where I graduated
+            with distinction. My professional adventures include working on
+            diverse projects like a Diabetes Dashboard management system and a
+            business trading system for the travel industry. I honed my skills
+            in Flutter, Firebase, and Bubble No-code, all while collaborating
+            with amazing teams and stakeholders. Recently, I&apos;ve been diving
+            deep into Next.js, Prisma ORM, Node.js, and more, developing dynamic
+            web platforms and ensuring secure, responsive user experiences.
+            Whether it&apos;s building from scratch or enhancing WordPress
+            sites, I thrive on delivering high-quality solutions. When I&apos;m
+            not coding, you&apos;ll find me exploring new tech trends,
+            optimizing workflows, and constantly learning. Join me on this
+            exciting journey as I share insights, tips, and tales from the world
+            of web development. Let&apos;s build something amazing together!
           </p>
-          <div className="p-4">
-            <Button>Read More</Button>
-          </div>
+          <div className="p-4">{/* <Button>Read More</Button> */}</div>
         </div>
       </div>
     </div>
