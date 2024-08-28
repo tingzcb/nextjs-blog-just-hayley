6b5c652d1a5f68a565fa2a7f85992e3b79263801
tingzcb <tingting.sun@cqumail.com>
2024-08-29 01:17:13 +1000

    footer update

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -3,7 +3,15 @@ import React from "react";
 type Props = {};
 
 const Footer = (props: Props) => {
-  return <div>Footer</div>;
+  return (
+    <div className="bg-white rounded-lg shadow m-4 dark:bg-gray-800">
+      <div className="w-full mx-auto max-w-screen-xl p-4 md:flex md:items-center md:justify-between">
+        <span className="text-sm text-gray-500 sm:text-center dark:text-gray-400">
+          © 2024 Tina.
+        </span>
+      </div>
+    </div>
+  );
 };
 
 export default Footer;
